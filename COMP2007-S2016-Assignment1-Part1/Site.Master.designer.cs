97897d6121c9ab059027e630ab37e243ab651d1e
Wangzhetuteng <ericdicaprio@outlook.com>
2016-05-30 13:34:24 -0400

    add Site.Master

--- COMP2007-S2016-Assignment1-Part1/Site.Master.designer.cs
+++ COMP2007-S2016-Assignment1-Part1/Site.Master.designer.cs
@@ -10,7 +10,25 @@
 namespace COMP2007_S2016_Assignment1_Part1 {
     
     
-    public partial class SiteMaster {
+    public partial class Site {
+        
+        /// <summary>
+        /// head control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+        
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
         /// MainContent control.
